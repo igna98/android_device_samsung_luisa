44169e7594f8253b098b06d8f8c3477cfb2bb62f
igna98 <igna.98@outlook.com>
2014-07-20 07:39:49 -0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,11 +24,11 @@ include device/samsung/bcm21553-common/BoardConfigCommon.mk
 
 # Board
 TARGET_BOARD_PLATFORM 				:= bcm21553
-TARGET_BOOTLOADER_BOARD_NAME 			:= totoro
+TARGET_BOOTLOADER_BOARD_NAME 			:= luisa
 
 # Kernel
-#TARGET_KERNEL_CONFIG				:= cyanogenmod_totoro_defconfig
-TARGET_PREBUILT_KERNEL				:= device/samsung/totoro/prebuilt/kernel
+#TARGET_KERNEL_CONFIG				:= cyanogenmod_luisa_defconfig
+TARGET_PREBUILT_KERNEL				:= device/samsung/luisa/prebuilt/kernel
 BOARD_KERNEL_BASE				:= 0x81600000
 BOARD_KERNEL_PAGESIZE				:= 4096
 BOARD_NAND_PAGE_SIZE				:= 4096 -s 128
@@ -36,6 +36,6 @@ BOARD_PAGE_SIZE					:= 0x00001000
 
 # Recovery
 BOARD_LDPI_RECOVERY				:= true
-BOARD_CUSTOM_RECOVERY_KEYMAPPING		:= ../../device/samsung/totoro/recovery/recovery_ui.c
-BOARD_CUSTOM_GRAPHICS				:= ../../../device/samsung/totoro/recovery/graphics.c
-TARGET_RECOVERY_FSTAB				:= device/samsung/totoro/recovery.fstab
+BOARD_CUSTOM_RECOVERY_KEYMAPPING		:= ../../device/samsung/luisa/recovery/recovery_ui.c
+BOARD_CUSTOM_GRAPHICS				:= ../../../device/samsung/luisa/recovery/graphics.c
+TARGET_RECOVERY_FSTAB				:= device/samsung/luisa/recovery.fstab
