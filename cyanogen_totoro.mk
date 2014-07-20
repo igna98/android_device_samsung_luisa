7503d31c6ea27390a5b22f0ddeb533a87105f180
igna98 <igna.98@outlook.com>
2014-07-20 07:32:46 -0300

    Update cyanogen_totoro.mk

--- cyanogen_totoro.mk
+++ cyanogen_totoro.mk
@@ -1,5 +1,5 @@
 # Inherit AOSP device configuration for blade.
-$(call inherit-product, device/samsung/totoro/device_totoro.mk)
+$(call inherit-product, device/samsung/luisa/device_luisa.mk)
 
 # Inherit some common cyanogenmod stuff.
 $(call inherit-product, vendor/cyanogen/products/common_full.mk)
@@ -13,12 +13,12 @@ $(call inherit-product, vendor/cyanogen/products/bcm_fm_radio.mk)
 #
 # Setup device specific product configuration.
 #
-PRODUCT_NAME := cyanogen_totoro
+PRODUCT_NAME := cyanogen_luisa
 PRODUCT_BRAND := samsung
-PRODUCT_DEVICE := totoro
-PRODUCT_MODEL := GT-S5360
+PRODUCT_DEVICE := luisa
+PRODUCT_MODEL := GT-B5510
 PRODUCT_MANUFACTURER := Samsung
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=GT-S5360 BUILD_ID=GRI40 BUILD_DISPLAY_ID=GWK74 BUILD_FINGERPRINT=samsung/GT-S5360/GT-S5360:2.3.6/GINGERBREAD/XWKTN:user/release-keys PRIVATE_BUILD_DESC="GT-S5360-user 2.3.6 GINGERBREAD XWKTN release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=GT-B5510 BUILD_ID=GINGERBREAD BUILD_DISPLAY_ID=GINGERBREAD.UBLE2 BUILD_FINGERPRINT=samsung/GT-B5510/GT-B5510:2.3.6/GINGERBREAD/UBLE2:user/release-keys PRIVATE_BUILD_DESC="GT-B5510-user 2.3.6 GINGERBREAD UBLE2 release-keys"
 
 # Extra overlay for MDPI
 PRODUCT_PACKAGE_OVERLAYS += vendor/cyanogen/overlay/ldpi
@@ -28,7 +28,7 @@ PRODUCT_COPY_FILES += \
      vendor/cyanogen/prebuilt/ldpi/media/bootanimation.zip:system/media/bootanimation.zip
 
 # Release name and versioning
-PRODUCT_RELEASE_NAME := GalaxyY
+PRODUCT_RELEASE_NAME := GalaxyYPro
 PRODUCT_VERSION_DEVICE_SPECIFIC :=
 -include vendor/cyanogen/products/common_versions.mk
 
