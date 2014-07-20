f33839428ae98f58500e1d9b7be2c936eb328a0f
igna98 <igna.98@outlook.com>
2014-07-20 07:43:55 -0300

    Update and rename device_totoro.mk to device_luisa.mk

--- device_luisa.mk
+++ device_luisa.mk
@@ -14,7 +14,7 @@
 
 # Inherit products
 $(call inherit-product, device/samsung/bcm21553-common/common.mk)
-$(call inherit-product, vendor/samsung/totoro/vendor_blobs.mk)
+$(call inherit-product, vendor/samsung/luisa/vendor_blobs.mk)
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
@@ -23,8 +23,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full.mk)
 
 # Add device package overlay
-DEVICE_PACKAGE_OVERLAYS += device/samsung/totoro/overlay
-PRODUCT_PACKAGE_OVERLAYS += vendor/cyanogen/overlay/mdpi
+DEVICE_PACKAGE_OVERLAYS += device/samsung/luisa/overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/cyanogen/overlay/ldpi
 
 # The gps config appropriate for this device
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
@@ -33,13 +33,13 @@ $(call inherit-product, device/common/gps/gps_us_supl.mk)
 PRODUCT_LOCALES += ldpi mdpi
 
 # Discard inherited values and use our own instead.
-PRODUCT_NAME := totoro
-PRODUCT_DEVICE := totoro
-PRODUCT_MODEL := GT-S5560
+PRODUCT_NAME := luisa
+PRODUCT_DEVICE := luisa
+PRODUCT_MODEL := GT-B5510
 
 # Prebuilt Kernel - DELETE from the package
 ifeq ($(TARGET_PREBUILT_KERNEL),)
-    LOCAL_KERNEL := device/samsung/totoro/prebuilt/kernel
+    LOCAL_KERNEL := device/samsung/luisa/prebuilt/kernel
 else
     LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
 endif
